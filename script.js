4ff2b31a75f6d623ad93875028a9c75ab25aa580
CapnCoin <reynoldsmichael95@gmail.com>
2025-09-23 22:43:57 +0200

    ADDED SIMPLE UI AND RE DID THE ENTIRE script.js
    TODO: add reset game button

--- script.js
+++ script.js
@@ -1,101 +1,194 @@
-const GameBoard = (function(){
-    let board = ['','','','','','','','','']
+function Player(marker, name){
+    return {marker, name};
+}
 
-    //return current board state
+const GameBoard = (function(){
+    let board = ['','','','','','','','',''];
+
+    /**
+     * pretty self explanitory don't you think?
+     * @returns the game board
+     */
     const getBoard = () => board;
 
-    //place marker on board
+    /**
+     * places a marker on the board.
+     * @param {number} index - index on the board to place the marker.
+     * @param {string} marker - marker to be placed.
+     * @returns {bool} true if marker has been placed. false if the marker could not be placed.
+     */
     const placeMarker = (index, marker) => {
         if(board[index] === ''){
             board[index] = marker;
-            return true; //successful placement of marker
+            return true;
         }
 
-        return false; //could not place marker
+        return false;
     }
 
-    //reset all board tiles to ''
+    /**
+     * resets all markers on the board
+     */
     const resetBoard = () => {
-        for(let i = 0; i < board.length; i ++){
-            board[i] = '';
-        }
+        board.forEach((index) => {
+            index = '';
+        });
     }
 
     return {getBoard, placeMarker, resetBoard};
 })();
 
-const Player = (name, marker) => {
-    return {name, marker};
-}
+const GameController = (function(){
+    const player1 = Player('X', 'Player1');
+    const player2 = Player('O', 'Player2');
 
-const GameControler = (function(){
-    const player1 = Player(1, 'X');
-    const player2 = Player(2, 'X');
+    let gameEnded = false;
+    let currentPlayer = player1;
 
-    let activePlayer = player1;
-    let gameOver = false;
+    const getCurrentPlayer = () => currentPlayer;
 
-    //return active player
-    const getActivePlayer = () => activePlayer;
-
-    //switch from current player to the next
+    /**
+     * switches between players
+     */
     const switchPlayer = () => {
-        activePlayer = activePlayer === player1 ? player2 : player1;
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
     }
 
+    /**
+     * 
+     * @returns 'X' or 'O' if there is a winner, 'tie' if it is a tie, or null if there is no winner or tie
+     */
     const checkForWin = () => {
         const board = GameBoard.getBoard();
-        //index positions to check for wins
+
         const winConditions = [
-            //horizontals
-            [0,1,2], [3,4,5], [6,7,8],
-            //verticals
-            [0,3,6], [1,4,7], [2,5,8],
-            //diagonals
-            [0,4,8], [2,4,6]
+            //horizontal
+            [0,1,2],[3,4,5],[6,7,8],
+            //vertical
+            [0,3,6],[1,4,7],[2,5,8],
+            //diagonal
+            [0,4,8],[2,4,6]
         ];
 
-        for(let condition in winConditions){
-            //unpack condition
-            const [a,b,c] = condition;
-
-            if(board[a] && board[a] === board[b] && board[a] === board[c]){
-                return board[a]; //returns 'X' or 'O' if there is a winner
+        for(const [a, b, c] of winConditions) {
+            if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+                return board[a]; // return 'X' or 'O' if there is a winner
             }
         }
 
-        if(!board.includes('')) return 'tie' // return 'tie' if it is a tie
-
-        return null; // return null if there is no winner yet
+        if(!board.includes('')) return 'tie'; //return 'tie' if it is a tie
+        
+        return null; //return null if there is no winner
     }
 
-    //main game logic
+    /**
+     * sets gameEnded to true if there is a winner or switches the player if not
+     * @param {number} index - index on board where player clicked
+     */
     const playRound = (index) => {
-        if(gameOver) return; //stop game if there is a winner or a tie
+        if(gameEnded) return; //exit if the game has already been won
 
-        //only take action if it is a valid play
-        if(GameBoard.placeMarker(index, getActivePlayer.marker)){
-            const winner = checkForWin();
+        const winVar = checkForWin();
 
-            if(winner){
-                gameOver = true;
-            }
-            else{
-                switchPlayer();
-            }
+        if(winVar){
+            gameEnded = true;
+            console.log(winVar);
+        }
+        else{
+            switchPlayer();
         }
     }
 
-    const restartGame = () => {
+    /**
+     * resets the board, current player, and gameEnded
+     */
+    const resetGame = () => {
         GameBoard.resetBoard();
-        activePlayer = player1;
-        gameOver = false;
+        currentPlayer = player1;
+        gameEnded = false;
     }
+
+    const getGameEnded = () => gameEnded;
+
+    return {playRound, resetGame, getCurrentPlayer, checkForWin, getGameEnded};
 })();
 
 const DisplayController = (function(){
-    //get DOM elements
-    //function to render the board
-    //function to update the status window
-    //function to update the game status (used to check for a win and detirmine what should be rendered in the status window)
+    //DOM elements
+    const statusWindow = document.getElementById('status-window');
+
+    const render = () => {
+        const container = document.getElementById('container');
+        const board = GameBoard.getBoard();
+
+        for(let i = 0; i < board.length; i++){
+            //create cell
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.dataset.index = i;
+            cell.textContent = board[i];
+
+            //add to container
+            container.appendChild(cell);
+        }
+    }
+
+    const refresh = () => {
+        const container = document.getElementById('container');
+
+        //clear all children
+        while(container.firstElementChild){
+            container.removeChild(container.firstElementChild);
+        }
+
+        render();
+    }
+
+    const updateStatusWindow = () => {
+        const winStatus = GameController.checkForWin();
+
+        switch (winStatus){
+            case 'X':
+                statusWindow.textContent = 'Player1 is the winner!';
+                break;
+
+            case 'O':
+                statusWindow.textContent = 'Player2 is the winner!';
+                break;
+
+            case 'tie':
+                case 'X':
+                statusWindow.textContent = "It's a tie!";
+                break;
+
+            case null:
+                case 'X':
+                statusWindow.textContent = `It's ${GameController.getCurrentPlayer().name}'s turn.`;
+                break;
+
+            default:
+                console.log('error in updateStatusWindow');
+        }
+    }
+
+    return {refresh, updateStatusWindow};
 })();
+
+document.addEventListener('click', (event) => {
+    if(event.target.classList.contains('cell')){
+        const cell = event.target;
+        const currentPlayer = GameController.getCurrentPlayer();
+        const index = cell.dataset.index;
+
+        if(GameBoard.placeMarker(index, currentPlayer.marker) && GameController.getGameEnded() === false){
+            cell.textContent = currentPlayer.marker;
+            GameController.playRound();
+            DisplayController.refresh();
+            DisplayController.updateStatusWindow();
+        }
+    }
+});
+
+//first render
+DisplayController.refresh();
+DisplayController.updateStatusWindow();
